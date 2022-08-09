0105ca116caa66f23238ef2b8679973dc5024a36
LinterestinG <50831121+cheerre56@users.noreply.github.com>
2022-08-09 21:41:51 +0800

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,3 +1,25 @@
+"""
+項目分工:
+    LIN:
+    添加UI:
+        無須一定要啟動Camera也能訓練模型
+        添加訊息框(訓練完成, 歡迎xx同學 等等...)
+    訓練模型模塊:
+                原本使用opencv掃臉模型改使用mediapipe識別截圖
+                添加目前拍到第幾張 and {學號}
+    識別模型模塊:
+                將原本的opencv改成用mediapipe截臉部
+    解決剛開始沒掃到臉崩潰的問題
+    -----
+    BOTI:
+    添加點名系統模塊:
+        添加資料庫
+        將名字寫入資料庫
+        掃臉成功識別後紀錄出席
+如有BUG麻煩回報
+"""
+
+
 import sys
 import tkinter as tk
 import tkinter.ttk as ttk
@@ -28,12 +50,14 @@ def student_ID():
     textExample.pack()
     btnRead=tk.Button(student_ID, height=1, width=10, text="Read", 
                         command=getTextInput)
-
     btnRead.pack()
+    student_ID.after(15000,student_ID.destroy)
     student_ID.mainloop()
 
 def Train():
     total = 10
+    #test 功能 紀錄拍了幾張 未來更新補充照片功能
+    testnub = str(total)
     mp_face_detection = mp.solutions.face_detection   # 建立偵測方法
     mp_drawing = mp.solutions.drawing_utils           # 建立繪圖方法
     if not os.path.exists("library"):                    # 如果不存在library資料夾
@@ -54,48 +78,51 @@ def Train():
                 print("此名字的人臉資料已經存在")
             else:
                 os.mkdir("library\\" + name)
+                test = open('library\\' + name + "\\" + "test.txt", 'w')
+                #test 功能 紀錄拍了幾張 未來更新補充照片功能
+                test.write(testnub)
+                test.close()
                 cap = cv2.VideoCapture(0)                       # 開啟攝影機
                 num = 1                                         # 影像編號
                 with mp_face_detection.FaceDetection(             # 開始偵測人臉
-        model_selection=0, min_detection_confidence=0.5) as face_detection:
+                model_selection=0, min_detection_confidence=0.5) as face_detection:
 
                     if not cap.isOpened():
                         print("Cannot open camera")
                         exit()
-                    while True:
+                    while num <= total:
                         ret, img = cap.read()
                         if not ret:
                             print("Cannot receive frame")
                             break
-                        img2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)   # 將 BGR 顏色轉換成 RGB
-                        results = face_detection.process(img2)        # 偵測人臉
-
-                        if results.detections:
-                            for detection in results.detections:
-                                bboxC = detection.location_data.relative_bounding_box
-                                ih, iw, ic = img.shape
-                                bbox = (int(bboxC.xmin * iw), int(bboxC.ymin * ih), int(bboxC.width * iw), int(bboxC.height * ih))
-                                cv2.rectangle(img, bbox, (255,255,255), 1)  # 標記人臉
-                                cv2.putText(img, f"Name: {name} Now{num}", (bbox[0], bbox[1]-20),cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 1, cv2.LINE_AA)
-                        cv2.imshow('oxxostudio', img)
-                        x = int(bboxC.xmin * iw)
-                        y = int(bboxC.ymin * ih)
-                        w = int(bboxC.width * iw)
-                        h = int(bboxC.height * ih)
-                        key = cv2.waitKey(200)
-                        if ret:                             # 讀取影像如果成功
-                            imageCrop = img[y:y+h,x:x+w]                     # 裁切
-                            imageResize = cv2.resize(imageCrop,(160,160))     # 重製大小
-                            faceName = "library\\" + name + "\\" + name + str(num) + ".jpg"
-                            cv2.imwrite(faceName, imageResize)      # 儲存人臉影像           
-                            if num >= total:                        # 拍指定人臉數後才終止               
-                                if num == total:
-                                    print(f"拍攝第 {num} 次人臉成功")
-                                break
-                            print(f"拍攝第 {num} 次人臉成功")
-                            num += 1
                         else:
-                            print("請露出人臉或者摘掉遮蔽物!")
+                            img2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)   # 將 BGR 顏色轉換成 RGB
+                            results = face_detection.process(img2)        # 偵測人臉
+
+                            if results.detections:
+                                for detection in results.detections:
+                                    bboxC = detection.location_data.relative_bounding_box
+                                    ih, iw, ic = img.shape
+                                    bbox = (int(bboxC.xmin * iw), int(bboxC.ymin * ih), 
+                                            int(bboxC.width * iw), int(bboxC.height * ih))
+                                    cv2.rectangle(img, bbox, (255,255,255), 1)  # 標記人臉
+                                    cv2.putText(img, f"Name: {name} Now{num}", (bbox[0], bbox[1]-20),
+                                                cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 1, cv2.LINE_AA)
+                                    cv2.imshow('oxxostudio', img)
+                                    x = int(bboxC.xmin * iw)
+                                    y = int(bboxC.ymin * ih)
+                                    w = int(bboxC.width * iw)
+                                    h = int(bboxC.height * ih) 
+                                    imageCrop = img[y:y+h,x:x+w]                     # 裁切
+                                    imageResize = cv2.resize(imageCrop,(160,160))     # 重製大小
+                                    faceName = "library\\" + name + "\\" + name +\
+                                                str(num) + ".jpg"
+                                    cv2.imwrite(faceName, imageResize)      # 儲存人臉影像   
+                                    key = cv2.waitKey(200)
+                                    print(f"拍攝第 {num} 次人臉成功")
+                                    num += 1
+                            else:
+                                print("請露出人臉或者摘掉遮蔽物!")
                 cap.release()
                 cv2.destroyAllWindows()
         else:
@@ -134,7 +161,9 @@ def Train():
 def Identify():
     mp_face_detection = mp.solutions.face_detection   # 建立偵測方法
     mp_drawing = mp.solutions.drawing_utils           # 建立繪圖方法
-
+    
+    if not os.path.exists("library\\deepmind.yml"):
+        messagebox.showerror( "Erro!","沒有人臉數據\n請點 [加入人臉模型]")
     model = cv2.face.LBPHFaceRecognizer_create()
     model.read('library\\deepmind.yml')                  # 讀取已訓練模型
     f = open('library\\employee.txt', 'r')               # 開啟姓名標籤
@@ -198,8 +227,8 @@ class Toplevel1:
            top is the toplevel containing window.'''
 
         top.geometry("566x265+723+328")
-        top.minsize(0, 0)
-        top.maxsize(1924, 1061)
+        top.minsize(566,265)
+        top.maxsize(566,265)
         top.resizable(1,  1)
         top.title("人臉點名系統")
         top.configure(background="#ffffff", highlightbackground="#d9d9d9", highlightcolor="black")
@@ -228,10 +257,9 @@ class Toplevel1:
 
 
 if __name__ == '__main__':
-    '''Main entry point for the application.'''
     global root
     root = tk.Tk()
-    root.protocol( 'WM_DELETE_WINDOW' , root.destroy)
+    root.protocol( 'WM_DELETE_WINDOW', root.destroy)
     # Creates a toplevel widget.
     global _top1, _w1
     _top1 = root
